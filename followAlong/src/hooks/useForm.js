4873d75d47951edf04b23813a65cdc31e186f226
Josh Jacobson <joshjacobson14@gmail.com>
2021-08-09 17:37:28 -0700

    lecture work-in-progress

--- followAlong/src/hooks/useForm.js
+++ followAlong/src/hooks/useForm.js
@@ -3,7 +3,12 @@ import { useState } from "react";
 // Custom hook to manage all state for a form with an arbitrary number of inputs
 const useForm = () => {
   const [firstName, setFirstName] = useState("");
-  return [firstName, setFirstName];
+
+  const handleChanges = e => {
+    setFirstName(e.target.value);
+  };
+
+  return [firstName, handleChanges];
 }
 
 export default useForm;

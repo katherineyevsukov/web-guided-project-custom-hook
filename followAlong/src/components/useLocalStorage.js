3fd6ff7a0596bc73218133e6a2bdac090a344fc2
Josh Jacobson <joshjacobson14@gmail.com>
2021-08-09 18:20:34 -0700

    lecture work-in-progress

---
+++ followAlong/src/components/useLocalStorage.js
@@ -0,0 +1,11 @@
+// Enhance useState with the ability to persist values beyond the current session.
+// If a user is filling out a form, for example, and accidentally closes the tab, they should be able to come back and pick up where they left off.
+
+import { useState } from "react";
+
+const useLocalStorage = () => {
+    return "something";
+
+}
+
+export default useLocalStorage
